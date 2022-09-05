2b77a96e597f03fe17a84909812b0127d6519789
kozlov artem <artemko1987@yandex.ru>
2022-09-05 09:25:29 +0300

    Добавил рабочий код

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[] {"hello","2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+int count =0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if(array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+    Console.WriteLine();
